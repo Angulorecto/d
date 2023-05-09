e4e65008991a9d3e7de7526f816faf35c4d7e063
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-05-09 12:10:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "node:path";
+import * as dotenv from "dotenv";
+
+const __dirname = process.cwd();
+const app = express(server);
+
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "index.html"));
+});
+
+app.get("/ok", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "ok.html"));
+});
+
+app.get("/404", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "404.html"));
+});
+
+app.get("/*", (req, res) => {
+  res.redirect("/404");
+});
